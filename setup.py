fdbdb160d0d030c9a9e444849c887e56920fa779
Rostyslav Yabchenko <rostyslav.yabchenko@gmail.com>
2021-01-03 13:52:47 +0200

    Int allowed(2)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 setup(
     name='Wrencher',
     packages=find_packages(include=['Wrench']),
-    version='0.1.5',
+    version='0.1.7',
     description='Cool lib',
     author='Rostcraft',
     license='MIT',
