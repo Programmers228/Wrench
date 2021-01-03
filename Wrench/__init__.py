787f6487f25b1561603f1adf95f2e6c44b48bd5c
Rostyslav Yabchenko <rostyslav.yabchenko@gmail.com>
2021-01-03 13:59:02 +0200

    Indentation level fix

--- Wrench/__init__.py
+++ Wrench/__init__.py
@@ -1,7 +1,7 @@
 def cround(a,b,action):
 	a1=str(float(a))
 	b1=str(float(b))
-    a1=a1.split(".")[1]
+	a1=a1.split(".")[1]
 	b1=b1.split(".")[1]
 	n=max(len(a1),len(b1))
 	return round(eval(str(a)+action+str(b)),n)
