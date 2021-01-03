622a5c1c2fee0120d57d9e93e5ed022413cb3717
rs-pro0 <55341323+rs-pro0@users.noreply.github.com>
2021-01-03 13:28:37 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import find_packages, setup
 setup(
-    name='Wrench',
+    name='Wrencher',
     packages=find_packages(include=['Wrench']),
     version='0.1.5',
     description='Cool lib',
